533937f757e4c4ef4065a7524582ede1cd0966a4
Cinar <sakcinar2006@gmail.com>
2022-02-07 21:18:34 +0300

    07/02/22

--- src/Blog/blog-app.js
+++
@@ -1,8 +0,0 @@
-let b = "far -fa-sun";
-let a = document.getElementById("dark-mode");
-a.addEventListener("click", function(e){
-  document.getElementsByTagName('body')[0].classList.toggle("dark-mode"); 
-  document.querySelector("#dark-mode").classList.toggle('fa-sun')
-  e.preventDefault();
-  
-});
